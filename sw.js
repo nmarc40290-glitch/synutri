9c440b63ae724c163e6ca1cd85c2fd8e2e7a055a
nmarc40290-glitch <nmarc64270@gmail.com>
2026-04-03 15:46:24 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Charge la version pour nommer le cache de façon unique
+importScripts('version.js');
+
+const CACHE_NAME = `synutri-v${VERSION}`;
+const ASSETS = [
+    './',
+    './index.html',
+    './app.js',
+    './version.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(
+        keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
+    )));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
